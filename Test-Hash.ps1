b8a2038c0716bf56d36c9b0b9a0b755ae46e4ab9
KozzyDaViking <kozzy222@gmail.com>
2023-04-16 10:46:12 +0930

    Added a script comparing cryptographic hashes.

---
+++ Test-Hash.ps1
@@ -0,0 +1,27 @@
+﻿param (
+    [Parameter(Mandatory=$true)][String]$fileDir,
+    [Parameter(Mandatory=$true)][String]$hash,
+    [String]$algorithm = "SHA256"
+)
+
+[Bool]$doesMatch = $false
+
+<# Get the file hashes
+    Get-FileHash will return the hash of the specified file using the supplied algorithm.
+    The supported algorithms include:
+        - SHA1
+        - SHA256 (Default)
+        - SHA384
+        - SHA512
+        - MD5
+#>
+
+$file = Get-ChildItem -Path $fileDir
+[Microsoft.PowerShell.Commands.FileHashInfo] $fileHashInfo = Get-FileHash $file -Algorithm $algorithm
+
+# Extract the hash info for message output purproses
+[String] $fileHash = $fileHashInfo.Hash
+
+if ( $fileHash -eq $hash ) { $doesMatch = $true }
+
+Write-Output "Does Source File: $file Hash: $fileHash match $hash ? $doesMatch"
